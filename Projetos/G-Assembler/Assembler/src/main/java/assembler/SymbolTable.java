2447e648c74f01411ba980285b17c24f2114fe37
jeronimo-a <jeronimoaa@al.insper.edu.br>
2021-11-13 16:45:21 -0300

    SymbolTable finalizado

--- Projetos/G-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
+++ Projetos/G-Assembler/Assembler/src/main/java/assembler/SymbolTable.java
@@ -29,6 +29,7 @@ public class SymbolTable {
      */
     public void addEntry(String symbol, int address) {
         /* TODO: implementar */
+        this.symbolTable.put(symbol, address);
     }
 
     /**
@@ -38,7 +39,7 @@ public class SymbolTable {
      */
     public Boolean contains(String symbol) {
         /* TODO: implementar */
-        return null;
+        return this.symbolTable.containsKey(symbol);
     }
 
     /**
@@ -48,7 +49,7 @@ public class SymbolTable {
      */
     public Integer getAddress(String symbol) {
         /* TODO: implementar */
-    	return null;
+        return this.symbolTable.get(symbol);
     }
 
     /**
@@ -61,6 +62,27 @@ public class SymbolTable {
     public void initialize() {
         // Exemplo: Registradores Virtuais
         this.addEntry("R0", 0);
+        this.addEntry("R1", 1);
+        this.addEntry("R2", 2);
+        this.addEntry("R3", 3);
+        this.addEntry("R4", 4);
+        this.addEntry("R5", 5);
+        this.addEntry("R6", 6);
+        this.addEntry("R7", 7);
+        this.addEntry("R8", 8);
+        this.addEntry("R9", 9);
+        this.addEntry("R10", 10);
+        this.addEntry("R11", 11);
+        this.addEntry("R12", 12);
+        this.addEntry("R13", 13);
+        this.addEntry("R14", 14);
+        this.addEntry("R15", 15);
+        this.addEntry("SCREEN", 16384);
+        this.addEntry("SP", 0);
+        this.addEntry("LCL", 1);
+        this.addEntry("ARG", 2);
+        this.addEntry("THIS", 3);
+        this.addEntry("THAT", 4);
         /* TODO: implementar */
     }
 }
